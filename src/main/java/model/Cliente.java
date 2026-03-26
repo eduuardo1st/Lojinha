8ce5b10467889b49cdb974fec04a98089c09738e
albernazz <davialbernaz19@gmail.com>
2026-03-26 19:31:23 -0300

    Criação da classe Pagamento.java e refatoração das demais classes presentes no pacote Model

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -1,34 +1,48 @@
 package model;
 
 public class Cliente {
-    private String nome;
-    private String email;
+   private int idCliente;
+   private String nome;
+   private String email;
+   private String endereco;
+   private String telefone;
+
+    public Cliente(int idCliente, String nome, String email, String endereco, String telefone) {
+        this.idCliente = idCliente;
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
 
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getEndereco() {
+        return endereco;
     }
 
-    public Cliente(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
+    public String getTelefone() {
+        return telefone;
     }
 
     @Override
     public String toString() {
-        return "Cliente = " +
-                "nome='" + nome + '\'' +
-                ", email='" + email;
+        return "Cliente{" +
+                "idCliente=" + idCliente +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
     }
 }
